48c295c03e0545abb36ad670d34093cf569bd3e6
cssidy <cassidy@cassidybrooke.net>
2017-03-20 23:04:45 -0400

    praw.ini

--- read_reply_bot.py
+++ read_reply_bot.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import praw
+import pprint
 
-reddit = praw.Reddit('bot')
+reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("askreddit")
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
+for submission in subreddit.hot(limit=10):
+    pprint.pprint("Title: ", submission.title)
+    pprint.pprint("Text: ", submission.selftext)
+    pprint.pprint("Score: ", submission.score)
